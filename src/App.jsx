653e89add1d2d3064f2aac9ea93d4c4fc5533381
mohammad-ardi <mdmiftahulislamardi@gmail.com>
2026-03-24 16:16:26 +0600

    navbar done, responsive (sm)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Navbar from './components/homepage/navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <h1 className='font-bold text-2xl'>Hi from app jsx</h1>
-      <button className='btn btn-primary'>Click</button>
+      <Navbar></Navbar>
     </>
   )
 }
---
+++ src/components/homepage/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import coinImg from '../../../assets/dollar-1.png'
+import navLogo from '../../../assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm flex flex-col sm:flex-row">
+            <div className="flex-1">
+                <img src={navLogo} alt="" />
+            </div>
+            <div className="flex items-center gap-4">
+                <a href="/home">Home</a>
+                <a href="/fixture">Fixture</a>
+                <a href="/teams">Teams</a>
+                <a href="/schedules">Schedules</a>
+                <button className="btn btn-ghostflex justify-center items-center gap-2">
+                    <span className='font-bold text-xl'>0 Coin</span>
+                    <img src={coinImg} alt="coin currency logo" />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
