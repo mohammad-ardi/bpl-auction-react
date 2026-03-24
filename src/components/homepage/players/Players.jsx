7613d896d840397ed2dc3fe82bb0afa0be7bbce0
mohammad-ardi <mdmiftahulislamardi@gmail.com>
2026-03-24 21:11:31 +0600

    button toggle added with useState, tab changing based on button toggle

--- src/components/homepage/players/Players.jsx
+++ src/components/homepage/players/Players.jsx
@@ -1,12 +1,29 @@
-import { use } from "react";
+import { use, useState } from "react";
 import AvailablePlayers from "./availablePlayers/AvailablePlayers";
+import SelectedPlayers from "./selectedPlayers/SelectedPlayers";
 
 const Players = ({playersPromise}) => {
+    const [btnState, setBtnState] = useState("available")
+
     const players = use(playersPromise)
     // console.log(data);
     return ( 
         <div>
-            <AvailablePlayers players={players}></AvailablePlayers>
+            <div className="container mx-auto my-8 flex justify-between items-center">
+                
+                {btnState === 'available' ? <p className="font-bold text-2xl">Available</p> : <p className="font-bold text-2xl">Selected Players</p>}
+
+                <div>
+                    <button
+                    onClick={()=>{setBtnState("available")}}
+                     className={`btn ${btnState === 'available' && 'bg-[#E7FE29]'} rounded-r-none rounded-l-xl`}>Available</button>
+                    <button 
+                    onClick={()=>{setBtnState("selected")}}
+                    className={`btn ${btnState === 'selected' && 'bg-[#E7FE29]'} rounded-l-none rounded-r-xl`}>Selected</button>
+                </div>
+            </div>
+
+            {btnState === 'available' ? <AvailablePlayers players={players}></AvailablePlayers> : <SelectedPlayers></SelectedPlayers>}
         </div>
      );
 }
---
+++ src/components/homepage/players/selectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,7 @@
+const SelectedPlayers = () => {
+    return ( 
+        <div>Seletced Players is gonna show up here</div>
+     );
+}
+ 
+export default SelectedPlayers;
